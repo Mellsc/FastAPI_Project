905c5e623576d49bee5763e613fe4760d0a48f2f
marianamelsilva819@gmail.com <marianamelsilva819@gmail.com>
2025-07-23 14:42:25 -0300

    feat: define função de usuário com id sequencial e adiciona no database

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -8,3 +8,11 @@ from fast_zero.schemas import UserDB, UserList, Message, UserPublic, UserSchema
 app = FastAPI()
 
 database = []
+
+
+@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+
+    database.append(user_with_id)
+    return user_with_id
