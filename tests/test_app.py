6e122f2520acd6e80e481f939abfd25eaa23fad4
marianamelsilva819@gmail.com <marianamelsilva819@gmail.com>
2025-07-24 10:09:57 -0300

    feat: implementa teste de integração para endpoint de criação de usuário (POST /users/)

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,3 +2,21 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
+
+
+def test_create_user(client: TestClient):
+    response = client.post(
+        "/users/",
+        json={
+            "username": "testusername",
+            "password": "password",
+            "email": "exemplo@gmail.com",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "username": "testusername",
+        "id": 1,
+        "email": "exemplo@gmail.com",
+    }
