6519c0de2ce9e7d73e694cd3e0353d52616f4ba4
marianamelsilva819@gmail.com <marianamelsilva819@gmail.com>
2025-07-24 09:35:15 -0300

    feat: adiciona função que deleta usuário e verifica se id existe no database

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -33,3 +33,15 @@ def update_user(user_id: int, user: UserSchema):
     user_with_id = UserDB(id=user_id, **user.model_dump())
     database[user_id - 1] = user_with_id
     return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail="User not found"
+        )
+    
+    del database[user_id - 1]
+
+    return {'message': 'User deleted!'}
