17554bd4d4dd55e756b456589db1cfb68f78ad54
marianamelsilva819@gmail.com <marianamelsilva819@gmail.com>
2025-09-03 15:07:25 -0300

    feat: cria modelo User para gerenciamento de registro de  usuários

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -1,7 +1,21 @@
-from sqlalchemy.orm import registry
+from datetime import datetime 
+from sqlalchemy.orm import (registry, Mapped, mapped_column)
 
 
 table_registry = registry()
 
 
+#tabela que gerencia o registro de usuarios num banco de dados
 @table_registry.mapped_as_dataclass
+class User:
+    __tablename__ = 'users'
+ 
+    id: Mapped[int] = mapped_column(init= False, primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+
+
