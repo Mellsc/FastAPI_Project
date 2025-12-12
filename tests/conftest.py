360ecaa9df76f67120562c8fb82adca8987d801d
marianamelsilva819@gmail.com <marianamelsilva819@gmail.com>
2025-12-12 10:44:29 -0300

    test: adiciona fixture db_session com SQLite em memória e StaticPool, além da fixture client que sobrescreve get_session para uso nos testes de API.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,39 @@
 import pytest
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import sessionmaker
-from fast_zero.models import table_registry
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
+
 from fast_zero.app import app
-
-
-
-@pytest.fixture()
-def client():
-    return TestClient(app)
+from fast_zero.database import get_session
+from fast_zero.models import table_registry
 
 
 @pytest.fixture()
-def session():
-    engine = create_engine()
+def db_session():
+    engine = create_engine(
+        "sqlite:///:memory:",
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool
+    )
+
     table_registry.metadata.create_all(engine)
 
-    with session(engine) as db_session:
-        yield db_session
+    with Session(engine) as session:
+        yield session
 
-    table_registry.metadata.drop_all(engine)
+
+@pytest.fixture()
+def client(db_session):
+    def session_test_mock():
+        return db_session
+
+    app.dependency_overrides[get_session] = session_test_mock
+
+    with TestClient(app) as client:
+        yield client
+
+    app.dependency_overrides.clear()
+
+
+
